06ae3691452a3fa5d5a13f45e8b734f9ddb94c12
DongHunWon <ehdgns5164@naver.com>
2021-09-01 14:27:08 +0900

    data error 처리

--- src/components/DataController.js
+++ src/components/DataController.js
@@ -3,6 +3,21 @@ export const saveData = (data) => {
 }
 
 export const loadData = () => {
-    const data = localStorage.getItem('data');
-    return data ? JSON.parse(data) : {todo: [], doing: [], done: []};
+    try {
+        const data = JSON.parse(localStorage.getItem('data'));
+        if (typeof data === 'boolean' ||
+            Array.isArray(data) ||
+            !('todo' in data && 'done' in data && 'doing' in data)
+        ) {
+            throw new Error();
+        }
+        return data;
+    } catch (error) {
+        return {
+            todo: [],
+            doing: [],
+            done: []
+        };
+    }
+    // return typeof data === 'object' ? JSON.parse(data) : {todo: [], doing: [], done: []};
 }
