1072186492db1b8fb36c1bbb4365582af4169c37
DongHunWon <ehdgns5164@naver.com>
2021-09-21 02:21:08 +0900

    code 수정

--- src/components/DataController.js
+++ src/components/DataController.js
@@ -1,17 +1,18 @@
+// localStorage에 데이터 저장
 export const saveData = (data) => {
     localStorage.setItem('data', JSON.stringify(data));
 }
 
+// localStorage에 데이터 불러오기
 export const loadData = () => {
     try {
+        // localStorage에 'data' 값 가져오기
         const data = JSON.parse(localStorage.getItem('data'));
-        if (typeof data === 'boolean' ||
-            Array.isArray(data) ||
-            !('todo' in data && 'done' in data && 'doing' in data)
-        ) {
-            throw new Error();
+        // data 형식 예외 처리
+        if ('todo' in data && 'done' in data && 'doing' in data) {
+            return data;
         }
-        return data;
+        throw new Error();
     } catch (error) {
         return {
             todo: [],
@@ -19,5 +20,4 @@ export const loadData = () => {
             done: []
         };
     }
-    // return typeof data === 'object' ? JSON.parse(data) : {todo: [], doing: [], done: []};
 }
